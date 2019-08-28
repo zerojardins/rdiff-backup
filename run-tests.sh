0cc9034d1d003c6e449d1b21fdf4259c3fb1ac81
Otto Kekäläinen <otto@seravo.com>
2019-08-29 00:00:05 +0300

    WIP: one script again to avoid building same docker image many times

--- run-tests.sh
+++ run-tests.sh
@@ -10,18 +10,18 @@ RDIFF_TEST_USER: $RDIFF_TEST_USER
 RDIFF_TEST_GROUP: $RDIFF_TEST_GROUP
 "
 
-# Debug: cache:
+# Debug cache:
 pwd
-ls -la ../
+ls -la ../rdiff-backup_testfiles
 
-if [ -d ../rdiff-backup_testfiles ]
+if [ -d ../rdiff-backup_testfiles/bigdir ]
 then
   echo "Test files found, not re-installng them.."
 else
   echo "Test files not found, installng them.."
   cd ..
   curl -LO https://github.com/ericzolf/rdiff-backup/releases/download/Testfiles2019-08-10/rdiff-backup_testfiles_2019-08-10.tar.gz
-  tar xvf *.tar.gz # This must be run as root
+  tar xvf rdiff-backup_testfiles_*.tar.gz # This must be run as root
   ./rdiff-backup_testfiles.fix.sh ${RDIFF_TEST_USER} ${RDIFF_TEST_GROUP} # This must be run as root
   cd rdiff-backup
 fi
