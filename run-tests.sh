ce5dd90ca476deff31a26462c777aadb93ab040f
Otto Kekäläinen <otto@seravo.com>
2019-08-28 22:22:06 +0300

    WIP: docker service and tests via file

---
+++ run-tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ..
+curl -LO https://github.com/ericzolf/rdiff-backup/releases/download/Testfiles2019-08-10/rdiff-backup_testfiles_2019-08-10.tar.gz
+tar xvf *.tar.gz # This must be run as root
+./rdiff-backup_testfiles.fix.sh ${RDIFF_TEST_USER} ${RDIFF_TEST_GROUP} # This must be run as root
+cd rdiff-backup
+tox -c tox.ini -e py37
+tox -c tox_root.ini -e py37 # This must be run as root
+tox -c tox_slow.ini -e py37
