d93bbf2cfd66f5c45c2d409abf6c8b4d0b0ae129
Otto Kekäläinen <otto@seravo.com>
2019-09-01 22:26:56 +0300

    WIP: fix caching

--- setup-testfiles.sh
+++ setup-testfiles.sh
@@ -11,8 +11,17 @@ else
   cd ..
   if [ ! -f rdiff-backup_testfiles.tar.gz ]
   then
-    rm -rf rdiff-backup_testfiles.tar.gz
-    curl -L https://github.com/ericzolf/rdiff-backup/releases/download/Testfiles2019-08-10/rdiff-backup_testfiles_2019-08-10.tar.gz --output rdiff-backup_testfiles.tar.gz
+    rm -rf rdiff-backup_testfiles.tar.gz # Clean away potential cruft
+    if [ -f cache/rdiff-backup_testfiles.tar.gz ]
+    then
+      echo "Using cached testfiles package"
+      mv -vf cache/rdiff-backup_testfiles.tar.gz .
+    else
+      echo "Downloading testfiles package"
+      curl -L https://github.com/ericzolf/rdiff-backup/releases/download/Testfiles2019-08-10/rdiff-backup_testfiles_2019-08-10.tar.gz --output rdiff-backup_testfiles.tar.gz
+      mkdir -p cache/
+      cp rdiff-backup_testfiles.tar.gz cache/
+    fi
   fi
   tar xf rdiff-backup_testfiles.tar.gz # This must be run as root
   ./rdiff-backup_testfiles.fix.sh "${RDIFF_TEST_USER}" "${RDIFF_TEST_GROUP}" # This must be run as root
