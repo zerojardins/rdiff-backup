7256ca62b5e9932989decba5b9f2fcd55d11f833
Otto Kekäläinen <otto@seravo.com>
2020-02-11 11:15:05 +0200

    WIP: Automatically publish branch ubuntu-ppa for Launchpad builder to consume

--- debian/autobuild.sh
+++ debian/autobuild.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -x
+
 # Automatically update changelog with new version number
 VERSION="$(./setup.py --version)"
 dch -b -v "${VERSION}" "Automatic build"
@@ -7,5 +10,15 @@ dch -b -v "${VERSION}" "Automatic build"
 # Build package ignoring the modified changelog
 gbp buildpackage -us -uc --git-ignore-new
 
-# Reset debian/changelog
+# If the build was successfull, rebase a new ubuntu-ppa branch and push it for Launchpad to consume
+git checkout -B ubuntu-ppa
+git add debian/changelog
+git commit -m "Automatic build"
+
+# @TODO: this would require SSH keys/permissions from inside the Docker container or Travis-CI runner,
+# thus will not work...
+git push --force --set-upstream origin ubuntu-ppa
+
+# Move back to previous branch and reset debian/changelog
+git checkout -
 git checkout debian/changelog
